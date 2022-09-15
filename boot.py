c2524d225226fee9809b917c0281537fffd97b9d
Yuki Koya <ykky@pepabo.com>
2022-09-15 15:43:34 +0900

    色々変更
    * キーマップを私の好みに
    * 起動時にUSBストレージとCDCを無効化

--- boot.py
+++ boot.py
@@ -1,4 +1,22 @@
 import supervisor
+import board
+import digitalio
+import storage
+import usb_cdc
+
 
 supervisor.set_next_stack_limit(4096 + 4096)
-# supervisor.disable_autoreload()
+
+# disable USB storage, CDC
+# see. http://kmkfw.io/docs/boot
+col = digitalio.DigitalInOut(board.GP5)
+row = digitalio.DigitalInOut(board.GP0)
+col.switch_to_output(value=True)
+row.switch_to_input(pull=digitalio.Pull.DOWN)
+
+if not row.value:
+  storage.disable_usb_drive()
+  usb_cdc.disable()
+
+row.deinit()
+col.deinit()
--- keymap.py
+++ keymap.py
@@ -1,35 +1,29 @@
 from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard
+import kmk.extensions.keymap_extras.keymap_jp
 
 layer1 = 0
 layer2 = 1
-layer3 = 2
-layer4 = 3
-layer5 = 4
-layer6 = 5
 
 ____ = KC.TRANSPARENT
 
 mo_layer2 = KC.MO(layer2)
-mo_layer3 = KC.MO(layer3)
-mo_layer4 = KC.MO(layer4)
-mo_layer5 = KC.MO(layer5)
-mo_layer6 = KC.MO(layer6)
+kc_paste = KC.LSFT(KC.INS)
 
 def layer1_keymap(keyboard: KMKKeyboard):
     left = [
         [KC.ESC,    KC.GRAVE,   KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5],
-        [           KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.N6],
-        [           KC.CAPS,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.LBRC],
-        [           KC.LSHIFT,  KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,       KC.RBRC],
-        [                                               KC.LCTL,    KC.LGUI,    KC.LALT,    KC.SPC],
+        [           KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,     kc_paste],
+        [           KC.LCTL,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,     KC.TILD],
+        [           KC.LSHIFT,  KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,     KC.LBRC],
+        [                                               KC.CAPS,    mo_layer2,  KC.LGUI,  KC.ENT],
     ]
     right = [
-        [           KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS,    KC.EQL],
-        [KC.BSPC,   KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.BSLS],
-        [KC.ENT,    KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT],
-        [KC.RSFT,   KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    mo_layer2],
-        [KC.RALT,   KC.RGUI,    KC.RCTL,    mo_layer2],
+        [           KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS],
+        [KC.PSCR,   KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.AT],
+        [KC.PIPE,   KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.COLN],
+        [KC.RBRC,   KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.UNDS],
+        [KC.SPC,    KC.BSPC,    KC.RCTL,    mo_layer2],
     ]
 
     return [[left, right]]
@@ -39,89 +33,15 @@ def layer2_keymap(keyboard: KMKKeyboard):
         [____,      ____,       KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5],
         [           ____,       ____,       ____,       ____,       ____,       ____,       KC.F6],
         [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       ____,       ____],
+        [           KC.LSHIFT,  ____,       ____,       ____,       ____,       ____,       ____],
+        [                                               ____,       ____,       KC.LGUI,    ____],
     ]
     right = [
         [           KC.F7,      KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.HOME,    KC.END],
-        [____,      KC.RGUI,    KC.APP,     KC.BSPC],
-    ]
-
-    return [[left, right]]
-
-def layer3_keymap(keyboard: KMKKeyboard):
-    left = [
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       ____,       ____],
-    ]
-    right = [
-        [           ____,       ____,       ____,       ____,      ____,        ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____],
-    ]
-
-
-    return [[left, right]]
-
-def layer4_keymap(keyboard: KMKKeyboard):
-    left = [
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       ____,       ____],
-    ]
-    right = [
-        [           ____,       ____,       ____,       ____,      ____,        ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____],
-    ]
-
-    return [[left, right]]
-
-def layer5_keymap(keyboard: KMKKeyboard):
-    left = [
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       ____,       ____],
-    ]
-    right = [
-        [           ____,       ____,       ____,       ____,      ____,        ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____],
-    ]
-
-    return [[left, right]]
-
-
-def layer6_keymap(keyboard: KMKKeyboard):
-    left = [
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       ____,       ____],
-    ]
-    right = [
-        [           ____,       ____,       ____,       ____,      ____,        ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____,       ____,       ____,       ____],
-        [____,      ____,       ____,       ____],
+        [____,      ____,       KC.PGDN,    KC.PGUP,    ____,       ____,       ____],
+        [____,      KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   ____,       ____],
+        [____,      KC.HOME,    ____,       ____,       KC.END,     ____,       ____],
+        [____,      KC.DEL,     ____,       ____],
     ]
 
     return [[left, right]]
@@ -129,11 +49,7 @@ def layer6_keymap(keyboard: KMKKeyboard):
 def get_keymap(keyboard: KMKKeyboard):
     layer1 = layer1_keymap(keyboard)
     layer2 = layer2_keymap(keyboard)
-    layer3 = layer3_keymap(keyboard)
-    layer4 = layer4_keymap(keyboard)
-    layer5 = layer5_keymap(keyboard)
-    layer6 = layer6_keymap(keyboard)
-    return [layer1[0], layer2[0], layer3[0], layer4[0], layer5[0], layer6[0]]
+    return [layer1[0], layer2[0]]
 
 def on_before_start(keyboard: KMKKeyboard):
     keyboard.pixels.set_rgb_fill((2, 2, 2))
