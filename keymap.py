9aa643b071d23f4e2d10c5f41512a2ef96650b04
Yuki Koya <ykky@pepabo.com>
2022-09-15 16:40:58 +0900

    ALTを有効活用したい

--- keymap.py
+++ keymap.py
@@ -17,14 +17,14 @@ def layer1_keymap(keyboard: KMKKeyboard):
         [           KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,     kc_paste],
         [           KC.LCTL,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,     KC.CIRC],
         [           KC.LSHIFT,  KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,     KC.LBRC],
-        [                                               KC.CAPS,    mo_layer2,  KC.LGUI,  KC.ENT],
+        [                                               KC.LALT,    mo_layer2,  KC.LGUI,  KC.ENT],
     ]
     right = [
         [           KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS],
         [KC.PSCR,   KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.AT],
         [KC.BSLS,   KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.COLN],
         [KC.RBRC,   KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.UNDS],
-        [KC.SPC,    KC.BSPC,    KC.RCTL,    mo_layer2],
+        [KC.SPC,    KC.BSPC,    mo_layer2,  KC.RCTL],
     ]
 
     return [[left, right]]
@@ -35,7 +35,7 @@ def layer2_keymap(keyboard: KMKKeyboard):
         [           ____,       ____,       ____,       ____,       ____,       ____,       KC.F6],
         [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
         [           KC.LSHIFT,  ____,       ____,       ____,       ____,       ____,       ____],
-        [                                               ____,       ____,       KC.LGUI,    ____],
+        [                                               KC.LALT,    ____,       KC.LGUI,    ____],
     ]
     right = [
         [           KC.F7,      KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12],
